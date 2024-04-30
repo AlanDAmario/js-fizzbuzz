ba2f545a9e685a482ea02093835ccc61b2d702f9
AlanDAmario <vega93@live.it>
2024-04-30 17:19:13 +0200

    [upd] bonus 1 added

--- js/main.js
+++ js/main.js
@@ -1,12 +1,24 @@
 'use strict';
 
 //MOLTIPLICATORE DA 1 A 100 3=fizz 5=buzz//
+const list = document.querySelector('.list')
+
+
 for (let i = 1; i <= 100; ++i) {
     let x = i.toString();
-    if (i % 3 === 0) {
+    // creazione del tag li <li class="fizzbuzz">fizz</li>
+    const li = document.createElement('li');
+
+    if ((i % 3 === 0) && (i % 5 === 0)) {
+        x = 'fizzbuzz';
+        // classe colore fizzbuzz
+    } else if (i % 3 === 0) {
         x = 'fizz';
     } else if (i % 5 === 0) {
         x = 'buzz';
     }
+    li.append(x);
     console.log(x);
+    list.append(li);
+    // append di li in list
 }
